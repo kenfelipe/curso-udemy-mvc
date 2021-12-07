10cf6620e7454ac39b2ba6dbdabd321d4294bb30
Felipe Kenzo <felipekenzohamada@gmail.com>
2021-12-07 16:56:18 -0300

    Initial commit

---
+++ App/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+class IndexController {
+
+  public function index() {
+    echo 'IndexController::index()';
+  }
+
+  public function about() {
+    echo 'IndexController::about()';
+  }
+}
+
+?>
