5de1d621630a158505ece7d3e532e0b0201f5780
Felipe Kenzo <felipekenzohamada@gmail.com>
2021-12-07 22:40:34 -0300

    Model

--- App/Controller/IndexController.php
+++ App/Controller/IndexController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+// MF frame
 use MF\Controller\Action;
+use MF\Model\Container;
+
+// Model
+use App\Model\Todo;
 
 class IndexController extends Action {
 
   public function index() {
-    $this->view->data = 'Index_Data';
+    $todos = Container::getModel('Todo');
+    $data = $todos->getTodos();
+
+    $this->view->data = $data;
+
     $this->render('layout', 'index');
   }
 
   public function about() {
-    $this->view->data = 'About_Data';
+    $todos = Container::getModel('Todo');
+    $data = $todos->getInfos();
+
+    $this->view->data = $data;
+
     $this->render('layout_2', 'about');
   }
 }
